5f49124c4cf76b2fb629e466db0408b75d3b422c
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2018-06-08 00:52:09 +0530

    getters added

--- src/components/ListComponent.vue
+++ src/components/ListComponent.vue
@@ -15,7 +15,7 @@ export default {
   },
   computed:{
     list(){
-      return this.$store.state.list;
+      return this.$store.getters.listOfItems;
     }
   }
 };
--- src/vuex/index.js
+++ src/vuex/index.js
@@ -7,6 +7,9 @@ const store = new Vuex.Store({
   state: {
     list: [{ name: 'apple', price: 40000 }, { name: 'mi', price: 20000 }],
   },
+  getters: {
+    listOfItems: state => state.list,
+  },
 });
 
 export default store;
