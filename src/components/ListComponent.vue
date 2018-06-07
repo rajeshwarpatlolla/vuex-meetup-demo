d1f6c74586efff082650c333ee6f0cf75531e87f
rajeshwarpatlolla <rajeshwar.patlolla@gmail.com>
2018-06-08 00:40:11 +0530

    passing props

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="ListComponent">
+		<h2>This is ListComponent</h2>
+    <div v-for="(item, index) of list" :key="index">
+      {{item.name}} - {{item.price}}
+    </div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ListComponent',
+	data() {
+		return {};
+  },
+  props:['list'],
+};
+</script>
+
+<style scoped>
+</style>
